2aebe09d35806d09f6f28c3e70fc8e344b1ddb3d
DESKTOP-APTFB7I\Joshua <hargobij@sheridancollege.ca>
2024-02-14 14:26:48 -0500

    this is task 1b

---
+++ Assets/Week 6/Scripts/Scene Name.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class SceneName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    TextMeshProUGUI sceneName;
+    void Start()
+    {
+        sceneName = GetComponent<TextMeshProUGUI>();
+        sceneName.text = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
